7fef8fd6ad4d90b2ef3faedd7d3211b86a6c8448
gcarreiras <gcarreiras@gmail.com>
2024-05-21 21:59:32 -0300

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
-import Home from './components/Home/Home';
+import Main from './components/Main/Main';
 
 function App() {
   return (
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Header />  
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Main />} />
         </Routes>
         <Footer />
       </Router>
