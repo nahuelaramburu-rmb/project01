bb7fb7285319caa8988e79f1c714160a0e2f938b
JGaston89 <gastonjurado89@gmail.com>
2024-05-24 10:38:08 -0300

    form coach created

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import emailjs from '@emailjs/browser';
 import contactUsImage from '../../assets/img/contact-us.jpg';
 
-
 const Form = () => {
   const form = useRef();
 
@@ -18,23 +17,23 @@ const Form = () => {
   };
 
   return (
- 
     <div className="d-flex justify-content-center align-items-center vh-100">
-      <div className="container d-flex justify-content-between align-items-center">
-        <div className="form-container p-4" style={{ width: '45%' }}>
+      <div className="container d-flex flex-wrap justify-content-between align-items-center">
+        <div className="form-container p-4" style={{ flex: '1 1 45%', minWidth: '300px' }}>
           <form ref={form} onSubmit={sendEmail} className="d-flex flex-column gap-3">
             <input type="text" name="user_name" placeholder="Nombre" className="form-control" required />
             <input type="text" name="user_lastName" placeholder="Apellido" className="form-control" required />
             <input type="email" name="user_email" placeholder="Correo" className="form-control" required />
-            <textarea name="message"  rows="7" placeholder="Mensaje" className="form-control" required></textarea>
+            <textarea name="message" rows="7" placeholder="Mensaje" className="form-control" required></textarea>
             <button type="submit" className="btn btn-dark mt-3">Send Message</button>
           </form>
         </div>
-        <div className="image-container d-flex justify-content-center align-items-center" style={{ width: '45%' }}>
+        <div className="image-container d-none d-md-flex justify-content-center align-items-center" style={{ flex: '1 1 45%', minWidth: '300px' }}>
           <img src={contactUsImage} alt="Photo" className="img-fluid" />
         </div>
       </div>
     </div>
   );
-}
+};
+
 export default Form;
