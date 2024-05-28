a09fe8389163fa1255b678659bfce5cef1e6a6fb
leonardo <leo.lp.ing@gmail.com>
2024-05-28 10:42:12 -0300

    Recomposicion de la Socialbar para evitar que linkedin quede abajo

--- src/components/SocialBar/SocialBar.jsx
+++ src/components/SocialBar/SocialBar.jsx
@@ -13,46 +13,54 @@ export default function SocialBar() {
   return (
     <div className="social-container m-auto">
       {/* <h3>Social Bar</h3> */}
+      <div className="row">
+        <div className="social-container m-auto">
+          <a href="https://wa.me/5492216254625" className="whatsapp social">
+            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+          </a>
 
-      <a href="https://wa.me/5492216254625" className="whatsapp social">
-        <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-      </a>
+          <a
+            href="mailto:ricardochianicoach@gmail.com"
+            className="gmail social"
+          >
+            <FontAwesomeIcon icon={faGoogle} size="2x" />
+          </a>
 
-      <a href="mailto:ricardochianicoach@gmail.com" className="gmail social">
-        <FontAwesomeIcon icon={faGoogle} size="2x" />
-      </a>
+          <a
+            href="https://www.youtube.com/@RicardoChianiCoach"
+            className="youtube social"
+          >
+            <FontAwesomeIcon icon={faYoutube} size="2x" />
+          </a>
 
-      <a
-        href="https://www.youtube.com/@RicardoChianiCoach"
-        className="youtube social"
-      >
-        <FontAwesomeIcon icon={faYoutube} size="2x" />
-      </a>
+          <a
+            href="https://www.instagram.com/ricardochianicoach/"
+            className="instagram social"
+          >
+            <FontAwesomeIcon icon={faInstagram} size="2x" />
+          </a>
 
-      <a
-        href="https://www.instagram.com/ricardochianicoach/"
-        className="instagram social"
-      >
-        <FontAwesomeIcon icon={faInstagram} size="2x" />
-      </a>
+          <a
+            href="https://www.facebook.com/profile.php?id=61553242420859&sk=about&locale=es_LA"
+            className="facebook social"
+          >
+            <FontAwesomeIcon icon={faFacebook} size="2x" />
+          </a>
+        </div>
+      </div>
 
-      <a
-        href="https://www.facebook.com/profile.php?id=61553242420859&sk=about&locale=es_LA"
-        className="facebook social"
-      >
-        <FontAwesomeIcon icon={faFacebook} size="2x" />
-      </a>
+      <div className="social-container m-auto">
+        <a
+          href="https://www.tiktok.com/@ricardochianicoach/"
+          className="tiktok social"
+        >
+          <FontAwesomeIcon icon={faTiktok} size="2x" />
+        </a>
 
-      <a
-        href="https://www.tiktok.com/@ricardochianicoach/"
-        className="tiktok social"
-      >
-        <FontAwesomeIcon icon={faTiktok} size="2x" />
-      </a>
-
-      <a href="https://www.linkedin.com/" className="linkedin social">
-        <FontAwesomeIcon icon={faLinkedin} size="2x" />
-      </a>
+        <a href="https://www.linkedin.com/" className="linkedin social">
+          <FontAwesomeIcon icon={faLinkedin} size="2x" />
+        </a>
+      </div>
 
       {/* <button type="button" class="btn btn-success">Success</button> */}
     </div>
