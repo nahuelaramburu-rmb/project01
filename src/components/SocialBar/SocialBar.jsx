ddbcf43ce04fddfd611df4390c15012fcf884d7b
leonardo <leo.lp.ing@gmail.com>
2024-05-27 23:13:28 -0300

    Agregado de links a redes sociales a la socialBar

--- src/components/SocialBar/SocialBar.jsx
+++ src/components/SocialBar/SocialBar.jsx
@@ -1,9 +1,12 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faWhatsapp,
+  faYoutube,
   faFacebook,
   faInstagram,
   faTiktok,
   faLinkedin,
+  faGoogle,
 } from "@fortawesome/free-brands-svg-icons";
 import "../SocialBar/css/SocialBar.css";
 export default function SocialBar() {
@@ -11,15 +14,39 @@ export default function SocialBar() {
     <div className="social-container m-auto">
       {/* <h3>Social Bar</h3> */}
 
-      <a href="https://www.instagram.com/" className="instagram social">
+      <a href="https://wa.me/5492216254625" className="whatsapp social">
+        <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+      </a>
+
+      <a href="mailto:ricardochianicoach@gmail.com" className="gmail social">
+        <FontAwesomeIcon icon={faGoogle} size="2x" />
+      </a>
+
+      <a
+        href="https://www.youtube.com/@RicardoChianiCoach"
+        className="youtube social"
+      >
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+
+      <a
+        href="https://www.instagram.com/ricardochianicoach/"
+        className="instagram social"
+      >
         <FontAwesomeIcon icon={faInstagram} size="2x" />
       </a>
 
-      <a href="https://www.facebook.com/" className="facebook social">
+      <a
+        href="https://www.facebook.com/profile.php?id=61553242420859&sk=about&locale=es_LA"
+        className="facebook social"
+      >
         <FontAwesomeIcon icon={faFacebook} size="2x" />
       </a>
 
-      <a href="https://www.tiktok.com/" className="tiktok social">
+      <a
+        href="https://www.tiktok.com/@ricardochianicoach/"
+        className="tiktok social"
+      >
         <FontAwesomeIcon icon={faTiktok} size="2x" />
       </a>
 
