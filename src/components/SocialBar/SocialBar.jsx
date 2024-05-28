5b60288f2de2cbe54e55894fcb8a54d00d7ef646
leonardo <leo.lp.ing@gmail.com>
2024-05-28 12:40:51 -0300

    Retiro del icono de Linkedin por que el cliente no tiene y jode le diseño en celular

--- src/components/SocialBar/SocialBar.jsx
+++ src/components/SocialBar/SocialBar.jsx
@@ -11,54 +11,44 @@ import {
 import "../SocialBar/css/SocialBar.css";
 export default function SocialBar() {
   return (
-    <div className="social-container m-auto">
+    <div className="container m-auto">
       {/* <h3>Social Bar</h3> */}
-      <div className="row">
-        <div className="social-container m-auto">
-          <a href="https://wa.me/5492216254625" className="whatsapp social">
-            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-          </a>
-
-          <a
-            href="mailto:ricardochianicoach@gmail.com"
-            className="gmail social"
-          >
-            <FontAwesomeIcon icon={faGoogle} size="2x" />
-          </a>
-
-          <a
-            href="https://www.youtube.com/@RicardoChianiCoach"
-            className="youtube social"
-          >
-            <FontAwesomeIcon icon={faYoutube} size="2x" />
-          </a>
-
-          <a
-            href="https://www.instagram.com/ricardochianicoach/"
-            className="instagram social"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2x" />
-          </a>
-
-          <a
-            href="https://www.facebook.com/profile.php?id=61553242420859&sk=about&locale=es_LA"
-            className="facebook social"
-          >
-            <FontAwesomeIcon icon={faFacebook} size="2x" />
-          </a>
-        </div>
-      </div>
 
       <div className="social-container m-auto">
+        <a href="https://wa.me/5492216254625" className="whatsapp social">
+          <FontAwesomeIcon icon={faWhatsapp} size="2x" />
+        </a>
+
+        <a href="mailto:ricardochianicoach@gmail.com" className="gmail social">
+          <FontAwesomeIcon icon={faGoogle} size="2x" />
+        </a>
+
+        <a
+          href="https://www.youtube.com/@RicardoChianiCoach"
+          className="youtube social"
+        >
+          <FontAwesomeIcon icon={faYoutube} size="2x" />
+        </a>
+
+        <a
+          href="https://www.instagram.com/ricardochianicoach/"
+          className="instagram social"
+        >
+          <FontAwesomeIcon icon={faInstagram} size="2x" />
+        </a>
+
+        <a
+          href="https://www.facebook.com/profile.php?id=61553242420859&sk=about&locale=es_LA"
+          className="facebook social"
+        >
+          <FontAwesomeIcon icon={faFacebook} size="2x" />
+        </a>
+
         <a
           href="https://www.tiktok.com/@ricardochianicoach/"
           className="tiktok social"
         >
           <FontAwesomeIcon icon={faTiktok} size="2x" />
-        </a>
-
-        <a href="https://www.linkedin.com/" className="linkedin social">
-          <FontAwesomeIcon icon={faLinkedin} size="2x" />
         </a>
       </div>
 
