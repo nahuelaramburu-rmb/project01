e0b790169d88dd58863e00317c03c13665d8f893
gcarreiras <gcarreiras86@gmail.com>
2024-05-29 18:30:26 -0300

    scroll in Main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,43 +1,19 @@
-import React, { useEffect } from "react";
+import React from "react";
 import CardMain from "../CardMain/CardMain";
 import SocialBar from "../SocialBar/SocialBar";
 import ContactForm from "../ContactForm/ContactForm";
 import "./Main.css";
 
 const Main = () => {
-  useEffect(() => {
-    const handleScroll = () => {
-      const sections = document.querySelectorAll(".section-scroll");
-      const scrollPosition = window.scrollY;
-      const viewportHeight = window.innerHeight;
-
-      sections.forEach((section, index) => {
-        if (
-          scrollPosition >= index * viewportHeight &&
-          scrollPosition < (index + 1) * viewportHeight
-        ) {
-          window.scrollTo({
-            top: index * viewportHeight,
-            behavior: "smooth",
-          });
-        }
-      });
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
     <div className="container-scroll">
       <div className="section-scroll">
         <CardMain />
+        <div className="spacer"></div> {/* Espaciador después de CardMain */}
       </div>
       <div className="section-scroll margin-bottom">
         <ContactForm />
+        <div className="spacer"></div> {/* Espaciador después de ContactForm */}
       </div>
       <div className="social-bar">
         <SocialBar />
