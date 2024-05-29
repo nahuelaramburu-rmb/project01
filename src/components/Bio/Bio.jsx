7b0c441f923dc0d30330db0685c11dc9fe399601
leonardo <leo.lp.ing@gmail.com>
2024-05-29 11:00:16 -0300

    creado componente bio

---
+++ src/components/Bio/Bio.jsx
@@ -0,0 +1,112 @@
+export default function Bio() {
+  return (
+    <div className="container">
+      <div>
+        <h2>FRASES QUE ME IDENTIFICAN:</h2>
+        <p>
+          Cuida tus pensamientos, porque se convertirán en tus palabras. Cuida
+          tus palabras, porque se convertirán en tus actos. Cuida tus actos,
+          porque convertirán en tus hábitos. Cuida tus hábitos, porque se
+          convertirán en tu destino. Gandhi.
+        </p>
+        <p>
+          Comienza haciendo lo necesario, luego lo posible y terminaras haciendo
+          lo imposible. San Francisco de Asís
+        </p>
+        <h3>Quién soy?</h3>
+        <p>
+          Durante 36 años, fui violinista en la Orquesta del Teatro Argentino de
+          La Plata, Buenos Aires, Argentina. Desde una edad temprana, comprendí
+          el valor de la disciplina, enfrenté desafíos y frustraciones, y
+          competí por alcanzar mis sueños. Al descubrir el coaching y la
+          Programación Neurolingüística (PNL), entendí cómo mis interpretaciones
+          de la realidad, mis patrones mentales y mis emociones condicionaban
+          mis resultados, relaciones y mi vida en general. Al identificar y
+          modificar estas limitaciones, descubrí nuevas oportunidades y
+          posibilidades para alcanzar mis objetivos. Implementando estas
+          herramientas tanto en mi vida personal como en la de mis alumnos de
+          violín, y en la asistencia a personas en sus vidas personales y
+          profesionales, confirmé que el cambio es posible. Hoy me dedico a
+          acompañar a personas, empresas y organizaciones en el logro de sus
+          objetivos, mientras disfrutan del proceso de caminar hacia el éxito.
+        </p>
+        <h3>Mi misión:</h3>
+        <p>
+          Es facilitar procesos de transformaciones profundas y duraderas, a
+          personas, empresas y organizaciones de todo el mundo, para maximizar
+          su potencial, logrando resultados extraordinarios y viviendo vidas
+          plenas y significativas. Enseñando principios universales y
+          estrategias efectivas, cultivando el liderazgo, integridad y
+          excelencia personal y profesional, para que puedan alcanzar sus metas
+          y sueños.
+        </p>
+        <h3>Mi Visión:</h3>
+        <p>
+          &quot;Crear una comunidad global de individuos y organizaciones que
+          operen con confianza, integridad y eficacia. Aspiro a ser reconocido
+          como un líder internacional en el desarrollo personal, organizacional
+          y empresarial, transformando vidas e inspirando a millones a vivir
+          vidas auténticas, plenas y extraordinarias, liderando un movimiento de
+          cambio positivo que permita a las personas alcanzar sus sueños y
+          mejorar el mundo que nos rodea. Visiono habitar un mundo más
+          consciente, compasivo y exitoso, en donde cada persona tenga las
+          herramientas para cambiar su vida, inspirar a otros a hacer lo mismo y
+          generar un impacto positivo y duradero en la sociedad.&quot;
+        </p>
+      </div>
+      <div>
+        <h2>FRASES QUE ME IDENTIFICAN:</h2>
+        <p>
+          Cuida tus pensamientos, porque se convertirán en tus palabras. Cuida
+          tus palabras, porque se convertirán en tus actos. Cuida tus actos,
+          porque convertirán en tus hábitos. Cuida tus hábitos, porque se
+          convertirán en tu destino. Gandhi.
+        </p>
+        <p>
+          Comienza haciendo lo necesario, luego lo posible y terminaras haciendo
+          lo imposible. San Francisco de Asís
+        </p>
+        <h3>Quién soy?</h3>
+        <p>
+          Durante 36 años, fui violinista en la Orquesta del Teatro Argentino de
+          La Plata, Buenos Aires, Argentina. Desde una edad temprana, comprendí
+          el valor de la disciplina, enfrenté desafíos y frustraciones, y
+          competí por alcanzar mis sueños. Al descubrir el coaching y la
+          Programación Neurolingüística (PNL), entendí cómo mis interpretaciones
+          de la realidad, mis patrones mentales y mis emociones condicionaban
+          mis resultados, relaciones y mi vida en general. Al identificar y
+          modificar estas limitaciones, descubrí nuevas oportunidades y
+          posibilidades para alcanzar mis objetivos. Implementando estas
+          herramientas tanto en mi vida personal como en la de mis alumnos de
+          violín, y en la asistencia a personas en sus vidas personales y
+          profesionales, confirmé que el cambio es posible. Hoy me dedico a
+          acompañar a personas, empresas y organizaciones en el logro de sus
+          objetivos, mientras disfrutan del proceso de caminar hacia el éxito.
+        </p>
+        <h3>Mi misión:</h3>
+        <p>
+          Es facilitar procesos de transformaciones profundas y duraderas, a
+          personas, empresas y organizaciones de todo el mundo, para maximizar
+          su potencial, logrando resultados extraordinarios y viviendo vidas
+          plenas y significativas. Enseñando principios universales y
+          estrategias efectivas, cultivando el liderazgo, integridad y
+          excelencia personal y profesional, para que puedan alcanzar sus metas
+          y sueños.
+        </p>
+        <h3>Mi Visión:</h3>
+        <p>
+          &quot;Crear una comunidad global de individuos y organizaciones que
+          operen con confianza, integridad y eficacia. Aspiro a ser reconocido
+          como un líder internacional en el desarrollo personal, organizacional
+          y empresarial, transformando vidas e inspirando a millones a vivir
+          vidas auténticas, plenas y extraordinarias, liderando un movimiento de
+          cambio positivo que permita a las personas alcanzar sus sueños y
+          mejorar el mundo que nos rodea. Visiono habitar un mundo más
+          consciente, compasivo y exitoso, en donde cada persona tenga las
+          herramientas para cambiar su vida, inspirar a otros a hacer lo mismo y
+          generar un impacto positivo y duradero en la sociedad.&quot;
+        </p>
+      </div>
+    </div>
+  );
+}
