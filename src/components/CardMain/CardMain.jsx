ff744392cc0587818dd11f9e6ad81edcd8fab11b
gcarreiras <gcarreiras86@gmail.com>
2024-05-30 09:14:58 -0300

    se adjunto la rama de tony, Agregado Caurusel y css form! !

--- src/components/CardMain/CardMain.jsx
+++ src/components/CardMain/CardMain.jsx
@@ -1,4 +1,5 @@
 import foto1 from "../../assets/imagenes/foto1.jpg";
+import Carrusel from "../Carrusel/Carrusel";
 
 export default function CardMain() {
   return (
@@ -15,6 +16,7 @@ export default function CardMain() {
         <div className="col-12 col-md-5 m-auto justify-content-center">
           <img src={foto1} alt="Foto del Cliente" style={{ width: "70%" }} />
         </div>
+        <Carrusel />
       </div>
     </div>
   );
