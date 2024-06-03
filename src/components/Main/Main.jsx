7c5abb03e9ab7c43e1b482407f77892bb77649df
leonardo <leo.lp.ing@gmail.com>
2024-06-03 19:56:21 -0300

    fixiando socialbar

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -8,23 +8,28 @@ import "./Main.css";
 const Main = () => {
   return (
     <div className="container-scroll">
+      <div className="Row "></div>
       <div id="inicio" className="section-scroll">
         <CardMain />
         <div className="spacer"></div>
       </div>
+
       <div id="clientes" className="section-scroll">
         <h2>Clientes</h2>
         <Carrusel />
         <div className="spacer"></div>
       </div>
+
       <div id="especializaciones" className="section-scroll">
         <h2>Especializaciones</h2>
         <div className="spacer"></div>
       </div>
+
       <div id="contacto" className="section-scroll margin-bottom">
         <ContactForm />
         <div className="spacer"></div>
       </div>
+
       <div className="social-bar" style={{ zIndex: 9999 }}>
         <SocialBar />
       </div>
