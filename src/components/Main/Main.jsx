97460d5b9b1e8d860f5914072a774f7fe95f83ec
leonardo <leo.lp.ing@gmail.com>
2024-06-06 11:20:36 -0300

    tickets 86a3pcjzg  y  86a3pcjew

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -5,19 +5,22 @@ import Carrusel from "../Carrusel/Carrusel";
 import ContactForm from "../ContactForm/ContactForm";
 import Bio from "../Bio/Bio";
 import "./Main.css";
-import CardRow from "../CardExt/CardRow";
+import CardExtRow from "../CardExt/CardExtRow";
+import CardExpRow from "../CardExp/CardExpRow";
 
 const Main = () => {
   return (
     <div className="container">
       <div className="Row "></div>
+
       <div id="inicio" className="col-12">
         <CardMain />
         <div className="minspacer"></div>
       </div>
+
       <div className="minspacer"></div>
-      <div id="inicio" className="col-12">
-        <CardRow />
+      <div id="newsection" className="col-12">
+        <CardExtRow />
         <div className="minspacer"></div>
       </div>
 
@@ -29,6 +32,12 @@ const Main = () => {
       <div id="bio" className="sec-box col-12">
         <h2>Bio</h2>
         <Bio />
+        <div className="minspacer"></div>
+      </div>
+
+      <div id="experiencias" className="sec-box col-12">
+        <h2>EXPERIENCIAS</h2>
+        <CardExpRow />
         <div className="minspacer"></div>
       </div>
 
