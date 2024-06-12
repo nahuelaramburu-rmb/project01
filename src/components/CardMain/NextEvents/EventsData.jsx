ef02f983bb58a038b897d2b137a3a03bd72b8547
leonardo <leo.lp.ing@gmail.com>
2024-06-12 13:33:27 -0300

    añadido carrusel de prosimos eventos

---
+++ src/components/CardMain/NextEvents/EventsData.jsx
@@ -0,0 +1,18 @@
+export const eventsData = [
+  {
+    ciudad1: "La Palta",
+    fecha1: "12/7/2024",
+    lugar1: "Estadio Unico",
+    ciudad2: "Quilmes",
+    fecha2: "12/8/2024",
+    lugar2: "Salon Los Abuelos",
+  },
+  {
+    ciudad1: "Espeleta",
+    fecha1: "5/9/2024",
+    lugar1: "Estadio Unico",
+    ciudad2: "Don Bosco",
+    fecha2: "25/9/2024",
+    lugar2: "El Aljive",
+  },
+];
